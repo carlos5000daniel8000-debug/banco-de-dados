67ab9df081c35f2fe3eadf98d6cc7250527b1acf
Nier_Paraiba <carlos5000daniel8000@gmail.com>
2026-01-30 19:57:24 -0300

    Java do começo
    Aprendiz

---
+++ main.java
@@ -0,0 +1,13 @@
+public Main{
+    public static void main (String[]args){
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome="Daniel o Lindo"
+        pessoa1.idade="30"
+        pessoa1.apresentar();
+
+        Pessoa pessoa2=new Pessoa();
+        pessoa2.nome="Carlos o Lindo"
+        pessoa2.idade="25"
+        pessoa2.apresentar();
+    }
+}
---
+++ pessoa.java
@@ -0,0 +1,5 @@
+puclic class Main {
+    puclic static void main (String[] args) {
+        system.out.println("Olá, mundo"):;
+    }
+}
